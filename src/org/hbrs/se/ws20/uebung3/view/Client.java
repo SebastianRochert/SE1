4601872fee54e338e8795485b9511690f82bb1a9
SebastianRochert <sebastian.rochert@t-online.de>
2020-11-22 17:53:38 +0100

    View Package ergänzt mit der Klasse Client.java

--- src/org/hbrs/se/ws20/uebung3/view/Client.java
+++ src/org/hbrs/se/ws20/uebung3/view/Client.java
@@ -1,4 +1,33 @@
 package src.org.hbrs.se.ws20.uebung3.view;
 
+import src.org.hbrs.se.ws20.uebung3.control.Container;
+import src.org.hbrs.se.ws20.uebung3.control.ContainerException;
+import src.org.hbrs.se.ws20.uebung3.control.Member;
+import src.org.hbrs.se.ws20.uebung3.control.MemberDef;
+
 public class Client {
+    public static void main(String[] args) {
+        Container con = Container.getInstance();
+
+        Client.fuellen(con);
+        Client.display(con);
+    }
+
+    public static void fuellen(Container con) {
+        Member x = new MemberDef(11);
+        Member y = new MemberDef(22);
+        Member z = new MemberDef(33);
+
+        try {
+            con.addMember(x);
+            con.addMember(y);
+            con.addMember(z);
+        } catch (ContainerException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void display(Container con) {
+            MemberView.dump(con.getCurrentList());
+    }
 }
--- src/org/hbrs/se/ws20/uebung3/view/MemberView.java
+++ src/org/hbrs/se/ws20/uebung3/view/MemberView.java
@@ -5,7 +5,7 @@ import src.org.hbrs.se.ws20.uebung3.control.Member;
 import java.util.List;
 
 public class MemberView {
-    public void dump(List<Member> liste) { //Ausgabe aller IDs der vorhandenen Member in der Console
+    public static void dump(List<Member> liste) { //Ausgabe aller IDs der vorhandenen Member in der Console
         System.out.print("Alle IDs der aktuell abgespeicherten Objekte im Container:\n");
         for(Member x:liste){
             System.out.print(x.toString());
