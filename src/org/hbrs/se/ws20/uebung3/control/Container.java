5123de99c2d0bccf959cb5d153171e06c9653fab
SebastianRochert <sebastian.rochert@t-online.de>
2020-11-24 18:43:32 +0100

    pss Objekt erstellt um auf die Methoden von PersistenceStrategyStream zuzugreifen.

--- src/org/hbrs/se/ws20/uebung3/control/Container.java
+++ src/org/hbrs/se/ws20/uebung3/control/Container.java
@@ -1,6 +1,7 @@
 package src.org.hbrs.se.ws20.uebung3.control;
 
 import src.org.hbrs.se.ws20.uebung3.persistence.PersistenceException;
+import src.org.hbrs.se.ws20.uebung3.persistence.PersistenceStrategy;
 import src.org.hbrs.se.ws20.uebung3.persistence.PersistenceStrategyStream;
 import src.org.hbrs.se.ws20.uebung3.view.MemberView;
 
@@ -9,15 +10,17 @@ import java.util.List;
 
 public class Container {
 
-    public List<Member> aList = null;
+    private List<Member> aList = null;
     /*
      Es soll zur Laufzeit zugesichert werden, dass von der Klasse Container nur ein einziges Mal ein Objekt erzeugt werden kann und somit nur ein
      Objekt davon im Speicher existiert.
      */
     private static Container instance; //instance = null
+    //Objekt der Klasse PersistenceStrategyStream um die dort definierten Methoden nutzen zu können
+    private final PersistenceStrategy<Member> pss = new PersistenceStrategyStream<Member>();
 
     private Container() {
-        aList = new ArrayList<>();
+        aList = new ArrayList<Member>();
     }
 
     public static synchronized Container getInstance() {
@@ -31,8 +34,9 @@ public class Container {
         instance = null;
     }
 
+
     //Uebung 2:
-        public void addMember(Member member) throws ContainerException {
+    public void addMember(Member member) throws ContainerException {
         /*
           ist das übergebene Member Objekt bereits in dem Container vorhanden? (Überprüfen)
           wenn ja, dann throw Exception vom Typ ContainerException
@@ -81,7 +85,7 @@ public class Container {
     }
 
     public List<Member> getCurrentList() {
-        return aList;
+        return this.aList;
     }
 
     public int size() { //Anzahl der Member in aList
@@ -91,16 +95,11 @@ public class Container {
     /*
     Uebung 3: Neue Methoden store() und load()
      */
-
     public void store() throws PersistenceException {
-//        try {
-//            PersistenceStrategyStream.save(aList);
-//        } catch (PersistenceException e) {
-//            throw e;
-//        }
+        pss.save(aList);
     }
 
     public void load() throws PersistenceException {
-//        this.aList = PersistenceStrategyStream.load();
+        aList = pss.load();
     }
 }
